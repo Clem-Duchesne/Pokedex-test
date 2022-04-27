564ab425720f37d71c7232808a6b848d5f0ec032
Clem-Duchesne <clemenceduchesne@orange.fr>
2022-04-27 18:57:23 +0200

    [component.ts] add filter of name, number and type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,20 +14,71 @@ import { PokeCard } from './models/poke-card.model';
 export class AppComponent {
   
   public pokemonList:{img:string,id:string,name:string, type:[string]}[] = pokemons;
+  public pokemonListByChange:{img:string,id:string,name:string, type:[string]}[] =[];
   public item!: PokeCard;
+
   private _url:string = "./assets/json/seed.json";
-
-
+  selectedType!: string;
+  inputName!:string;
+  inputId!:number;
+  
   ngOnInit(): void {
 
   }
-  onNameChange(): void {
+  onNameChange(event : any): void { 
+    if(event.target.value != ""){
+      this.pokemonList = pokemons;
+      this.inputName = event.target.value.toLowerCase();
+      this.pokemonListByChange = [];
+      this.pokemonList.forEach((mypokemon) =>{
+        if(mypokemon.name.toLowerCase().includes(this.inputName))
+          this.pokemonListByChange.push(mypokemon);
+          
+      });
+      this.pokemonList = this.pokemonListByChange;
+    }
+    else{
+      this.pokemonList = pokemons;
+    }
+      
+  }
+  onNumberChange(event : any):void {
+    if(event.target.value != ""){
+      this.pokemonList = pokemons;
+      this.inputId = event.target.value;
+      this.pokemonListByChange = [];
+      this.pokemonList.forEach((mypokemon) =>{
+        var pokemonId = Number(mypokemon.id);
+        if(pokemonId == this.inputId)
+          this.pokemonListByChange.push(mypokemon);
+          
+      });
+      this.pokemonList = this.pokemonListByChange;
+    }
+    else{
+      this.pokemonList = pokemons;
+    }
 
   }
-  onNumberChange():void {
+  onSelectedTypeValue(event: any) :void{
+    if(event.target.value != "default"){
+      this.selectedType = event.target.value;
+      this.pokemonListByChange = [];
+      this.pokemonList.forEach((mypokemon) =>{
+        mypokemon.type.forEach((t)=>{
+          if(t == this.selectedType)
+          this.pokemonListByChange.push(mypokemon);
+        
+        });
+      });
+      this.pokemonList = this.pokemonListByChange;
+    }
+    else{
+      this.pokemonList = pokemons;
+    }
+      
+      
     
-  }
-  onSelectedTypeValue(): void{
     
   }
 }
