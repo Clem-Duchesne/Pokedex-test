12eb26b4d08cde3e57d247fcaab9cfb5357224bb
Clem-Duchesne <clemenceduchesne@orange.fr>
2022-05-02 11:03:56 +0200

    [ts component] add multiple search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ import { PokeCard } from './models/poke-card.model';
 export class AppComponent {
   
   public pokemonList:{img:string,id:string,name:string, type:[string]}[] = pokemons;
+  public pokemonListBySelect:{img:string,id:string,name:string, type:[string]}[] =[];
   public pokemonListByChange:{img:string,id:string,name:string, type:[string]}[] =[];
   public item!: PokeCard;
 
@@ -27,33 +28,74 @@ export class AppComponent {
   }
   onNameChange(event : any): void { 
     if(event.target.value != ""){
-      this.pokemonList = pokemons;
-      this.inputName = event.target.value.toLowerCase();
-      this.pokemonListByChange = [];
-      this.pokemonList.forEach((mypokemon) =>{
-        if(mypokemon.name.toLowerCase().includes(this.inputName))
-          this.pokemonListByChange.push(mypokemon);
-          
-      });
-      this.pokemonList = this.pokemonListByChange;
+      if(this.pokemonListBySelect.length != 0){
+        
+          this.pokemonList = pokemons;
+          this.inputName = event.target.value.toLowerCase();
+          this.pokemonListByChange = [];
+          this.pokemonListBySelect.forEach((mypokemon) =>{
+            var pokemonId = Number(mypokemon.id);
+            if(mypokemon.name.toLowerCase().includes(this.inputName))
+            this.pokemonListByChange.push(mypokemon);
+              
+          });
+          this.pokemonList = this.pokemonListByChange;
+      }
+      else{
+        
+          this.pokemonList = pokemons;
+          this.inputName = event.target.value.toLowerCase();
+          this.pokemonListByChange = [];
+          this.pokemonList.forEach((mypokemon) =>{
+            if(mypokemon.name.toLowerCase().includes(this.inputName))
+              this.pokemonListByChange.push(mypokemon);
+              
+          });
+          this.pokemonList = this.pokemonListByChange;
+      }
+    }
+    
+    else if(event.target.value == "" && this.pokemonListBySelect.length!=0){
+      this.pokemonList = this.pokemonListBySelect;
     }
     else{
       this.pokemonList = pokemons;
     }
+    
       
   }
   onNumberChange(event : any):void {
     if(event.target.value != ""){
-      this.pokemonList = pokemons;
-      this.inputId = event.target.value;
-      this.pokemonListByChange = [];
-      this.pokemonList.forEach((mypokemon) =>{
-        var pokemonId = Number(mypokemon.id);
-        if(pokemonId == this.inputId)
-          this.pokemonListByChange.push(mypokemon);
-          
-      });
-      this.pokemonList = this.pokemonListByChange;
+      if(this.pokemonListBySelect.length != 0){
+        
+          this.pokemonList = pokemons;
+          this.inputId = event.target.value;
+          this.pokemonListByChange = [];
+          this.pokemonListBySelect.forEach((mypokemon) =>{
+            var pokemonId = Number(mypokemon.id);
+            if(pokemonId == this.inputId)
+              this.pokemonListByChange.push(mypokemon);
+              
+          });
+          this.pokemonList = this.pokemonListByChange;
+        
+      }
+      else{
+          this.pokemonList = pokemons;
+          this.inputId = event.target.value;
+          this.pokemonListBySelect = [];
+          this.pokemonList.forEach((mypokemon) =>{
+            var pokemonId = Number(mypokemon.id);
+            if(pokemonId == this.inputId)
+              this.pokemonListBySelect.push(mypokemon);
+              
+          });
+          this.pokemonList = this.pokemonListBySelect;
+        
+      }
+    }
+    else if(event.target.value == "" && this.pokemonListBySelect.length!=0){
+      this.pokemonList = this.pokemonListBySelect;
     }
     else{
       this.pokemonList = pokemons;
@@ -63,15 +105,15 @@ export class AppComponent {
   onSelectedTypeValue(event: any) :void{
     if(event.target.value != "default"){
       this.selectedType = event.target.value;
-      this.pokemonListByChange = [];
+      this.pokemonListBySelect = [];
       this.pokemonList.forEach((mypokemon) =>{
         mypokemon.type.forEach((t)=>{
           if(t == this.selectedType)
-          this.pokemonListByChange.push(mypokemon);
+          this.pokemonListBySelect.push(mypokemon);
         
         });
       });
-      this.pokemonList = this.pokemonListByChange;
+      this.pokemonList = this.pokemonListBySelect;
     }
     else{
       this.pokemonList = pokemons;
